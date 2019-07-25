5fbc0adbd59345fb0743c2976d612b8c350e606d
Paul Raab <pvraab@hotmail.com>
2019-07-25 13:27:45 -0600

    Add notes functionality

--- public/assets/js/articles.js
+++ public/assets/js/articles.js
@@ -1,5 +1,7 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+  var currentId;
+
   $(".delete-article").on("click", function(event) {
     console.log("Delete click");
     var id = $(this).data("id");
@@ -12,7 +14,6 @@ $(function() {
       // Reload the page to get the updated list
       location.reload();
     });
-
   });
 
   $(".save-it").on("click", function(event) {
@@ -27,19 +28,66 @@ $(function() {
       // Reload the page to get the updated list
       location.reload();
     });
-
   });
 
-  $(".delete-burger").on("click", function(event) {
+  $(".note-it").on("click", function(event) {
+    console.log("Note It click");
     var id = $(this).data("id");
+    console.log(id);
+    currentId = id;
+    // Clear the form to start
+    $("#db-notes").val("");
+    $("#new-note").val("");
 
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(function() {
-      console.log("Deleted burger", id);
-      // Reload the page to get the updated list
-      location.reload();
-    });
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "GET",
+      url: "/populatedarticle/" + currentId
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log("Get notes");
+        console.log(data);
+        console.log(data[0].notes[0].body);
+        // Clear the form after submitting
+        var outStr = "";
+        data[0].notes.forEach(element => {
+          console.log(element);
+          outStr = outStr + element.body + "\n";
+        });
+        $("#db-notes").val(outStr);
+      });
+
+    $("#noteModal").modal("show");
+  });
+
+  // Add a note
+  $("#noteBtn").on("click", function(event) {
+    event.preventDefault();
+
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + currentId,
+      data: {
+        // Value taken from note textarea
+        body: $("#new-note")
+          .val()
+          .trim()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log("Added a new note");
+        console.log(data);
+        // Clear the form after submitting
+        $("#db-notes").val("");
+        $("#new-note").val("");
+
+        $("#noteModal").modal("hide");
+        location.reload();
+      });
   });
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -83,11 +83,43 @@ module.exports = function(app) {
 
   // Route for saving/updating an Article's associated Note
   app.post("/articles/:id", function(req, res) {
+    console.log("Post note");
+    console.log(req.params.id);
+    console.log(req.body);
+
+    // Create a new Note in the db
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article (there's only one) and push the new Note's _id to the Article's `notes` array
+        // { new: true } tells the query that we want it to return the updated Article -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate(
+          { _id: req.params.id },
+          { $push: { notes: dbNote._id } },
+          { new: true }
+        );
+      })
+      .then(function(dbArticle) {
+        // If the Article was updated successfully, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurs, send it back to the client
+        res.json(err);
+      });
+  });
+
+  // Route for saving/updating an Article's associated Note
+  // This one only does one note per article
+  app.post("/articles_org/:id", function(req, res) {
+    console.log("Post note");
+    console.log(req.params.id);
+    console.log(req.body);
     // Create a new note and pass the req.body to the entry
-    db.Article.create(req.body)
+    db.Note.create(req.body)
       .then(function(dbNote) {
         // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // { new: true } tells the query that we want it to return the updated Article -- it returns the original by default
         // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
         return db.Article.findOneAndUpdate(
           { _id: req.params.id },
@@ -106,6 +138,7 @@ module.exports = function(app) {
   });
 
   // Route for updating a saved Article to the db
+  // Right now to mark it as saved
   app.put("/articles/:id", function(req, res) {
     // Create a new Article using the `result` object built from scraping
     db.Article.updateOne(
@@ -148,11 +181,40 @@ module.exports = function(app) {
     );
   });
 
+  // Route to get a Article by id and populate it with their notes
+  app.get("/populatedarticle/:id", function(req, res) {
+    // Find an article
+    db.Article.find({ _id: mongojs.ObjectID(req.params.id) })
+      // Specify that we want to populate the retrieved articles with any associated notes
+      .populate("notes")
+      .then(function(dbArticle) {
+        // If able to successfully find and associate all Articles and Notes, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurs, send it back to the client
+        res.json(err);
+      });
+  });
+
+  // Route for saving a Note
+  app.post("/api/notes", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        res.json(dbNote);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
   // Clear DB
   app.get("/clear", function(req, res) {
     console.log("In clear API");
     // Remove a note using the objectID
-    db.Article.deleteMany( function(error, removed) {
+    db.Article.deleteMany(function(error, removed) {
       // Log any errors from mongojs
       if (error) {
         console.log(error);
